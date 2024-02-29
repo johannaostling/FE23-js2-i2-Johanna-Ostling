13acbf15709ae6e41695a668b44f3320c9dcdb1f
johannaostling <johanna.ostling@gritacademy.se>
2024-02-29 14:22:52 +0100

    added get function with display

--- index.js
+++ index.js
@@ -5,11 +5,43 @@ const header = {
 }
 
 
-const addBtn = document.querySelector("#add")
-const todocon = document.querySelector("#todo")
-const form = document.querySelector("form");
-addBtn.addEventListener('submit', (event)=>{
-    event.preventDefault()
-    console.log("add")
+const formEl = document.querySelector("form")
 
-})
+formEl.addEventListener("submit", handleform)
+function handleform(event){
+    event.preventDefault()
+    console.log("i handleForm")
+
+    get()
+}
+
+const todoContainer = document.querySelector("#todo")
+const inprogContainer = document.querySelector("#inprog")
+const doneContainer = document.querySelector("#done")
+async function get(){
+    const url= baseUrl + ".json";
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data)
+
+    for (const key in data) {
+        console.log(key, data[key].task);
+        const name = data[key].task;
+        const keyEl = document.createElement("h2");
+        keyEl.id = key;
+        keyEl.innerText = name
+
+
+        if (data[key].status == "to do"){
+
+
+            todoContainer.append(keyEl)
+        }
+        else if(data[key].status == "in progress"){
+            inprogContainer.append(keyEl)
+        }
+        else if(data[key].status == "done")
+            doneContainer.append(keyEl)
+        
+    }
+}
