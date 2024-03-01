76a10203e38e060032c6a3a04d9b6cc7aa18ae1d
johannaostling <johanna.ostling@gritacademy.se>
2024-03-01 11:05:31 +0100

    Fixed some things

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { get } from "./module/getfunc.js"
 const baseUrl = 'https://scrum-board-4eb67-default-rtdb.europe-west1.firebasedatabase.app/tasks'
 
 const header = {
@@ -15,33 +16,33 @@ function handleform(event){
     get()
 }
 
-const todoContainer = document.querySelector("#todo")
-const inprogContainer = document.querySelector("#inprog")
-const doneContainer = document.querySelector("#done")
-async function get(){
-    const url= baseUrl + ".json";
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data)
+// const todoContainer = document.querySelector("#todo")
+// const inprogContainer = document.querySelector("#inprog")
+// const doneContainer = document.querySelector("#done")
+// async function get(){
+//     const url= baseUrl + ".json";
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     console.log(data)
 
-    for (const key in data) {
-        console.log(key, data[key].task);
-        const name = data[key].task;
-        const keyEl = document.createElement("h2");
-        keyEl.id = key;
-        keyEl.innerText = name
-
-
-        if (data[key].status == "to do"){
+//     for (const key in data) {
+//         console.log(key, data[key].task);
+//         const name = data[key].task;
+//         const keyEl = document.createElement("h2");
+//         keyEl.id = key;
+//         keyEl.innerText = name
 
 
-            todoContainer.append(keyEl)
-        }
-        else if(data[key].status == "in progress"){
-            inprogContainer.append(keyEl)
-        }
-        else if(data[key].status == "done")
-            doneContainer.append(keyEl)
+//         if (data[key].status == "to do"){
+
+
+//             todoContainer.append(keyEl)
+//         }
+//         else if(data[key].status == "in progress"){
+//             inprogContainer.append(keyEl)
+//         }
+//         else if(data[key].status == "done")
+//             doneContainer.append(keyEl)
         
-    }
-}
+//     }
+// }
---
+++ module/getfunc.js
@@ -0,0 +1,33 @@
+const baseUrl = 'https://scrum-board-4eb67-default-rtdb.europe-west1.firebasedatabase.app/tasks'
+
+const todoContainer = document.querySelector("#todo")
+const inprogContainer = document.querySelector("#inprog")
+const doneContainer = document.querySelector("#done")
+
+async function get(){
+    const url= baseUrl + ".json";
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data)
+
+    for (const key in data) {
+        console.log(key, data[key].task);
+        const name = data[key].task;
+        const keyEl = document.createElement("h2");
+        keyEl.id = key;
+        keyEl.innerText = name
+
+
+        if (data[key].status == "to do"){
+            todoContainer.append(keyEl)
+        }
+        else if(data[key].status == "in progress"){
+            inprogContainer.append(keyEl)
+        }
+        else if(data[key].status == "done"){
+            doneContainer.append(keyEl)
+        }
+            
+    }
+}
+export {get}
